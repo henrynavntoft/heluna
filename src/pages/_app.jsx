a270ae359aa6a7f5aaa335dc99ec06de6acfcb32
Henry Navntoft <henrylnavntoft@gmail.com>
2023-06-11 14:50:10 +0300

    Added GTAG another page.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,26 +1,9 @@
 import "@/styles/globals.css";
 import "typeface-open-sans";
-import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="font-sans">
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script id="gtag-manager" strategy="lazyOnload">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
-
       <Component {...pageProps} />
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,26 @@
 import Layout from "@/components/Layout";
 import CardComponent from "@/components/CardComponent";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="gtag-manager" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Head>
         <title>HELUNA | AGENCY</title>
         <meta
