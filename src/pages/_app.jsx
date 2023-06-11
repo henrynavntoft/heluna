1729e6b6cd54cc8f3d4e406db41931ab539c9441
Henry Navntoft <henrylnavntoft@gmail.com>
2023-06-11 15:17:29 +0300

    GTAG

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,24 +1,13 @@
 import "@/styles/globals.css";
 import "typeface-open-sans";
-import Script from "next/script";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="font-sans">
-      <Component {...pageProps} />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-MJBN3X1TSW"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+      <GoogleAnalytics measurementId="G-MJBN3X1TSW" />
 
-          gtag('config', 'G-MJBN3X1TSW');
-        `}
-      </Script>
+      <Component {...pageProps} />
     </div>
   );
 }
