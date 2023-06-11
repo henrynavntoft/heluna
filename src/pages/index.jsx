704fafe3419f17e1469b66db1a997d8f70231e5e
Henry Navntoft <henrylnavntoft@gmail.com>
2023-06-11 15:01:28 +0300

    GTAG FIX

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,20 +7,19 @@ export default function Home() {
   return (
     <div>
       <Script
-        async
+        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-MJBN3X1TSW"
       />
 
       <Script id="G-MJBN3X1TSW">
         {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-  gtag('config', 'G-MJBN3X1TSW');
-`}
+      gtag('config', 'G-MJBN3X1TSW');
+    `}
       </Script>
-
       <Head>
         <title>HELUNA | AGENCY</title>
         <meta
